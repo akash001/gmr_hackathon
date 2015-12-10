14607b9c475b6f7ca169c6a67f9868f7b0b6bc67
akash001 <abhijithk@gmail.com>
2015-12-09 17:53:40 -0800

    Persisting login and partial integration with Sivas web service.

--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,81 +1,22 @@
 angular.module('starter.services', [])
 
-//.service('LoginService', function($q) {
-//    return {
-//        loginUser: function(name, pw) {
-//            var deferred = $q.defer();
-//            var promise = deferred.promise;
-// 
-//            if (name == 'user' && pw == 'secret') {
-//                deferred.resolve('Welcome ' + name + '!');
-//            } else {
-//                deferred.reject('Wrong credentials.');
-//            }
-//            promise.success = function(fn) {
-//                promise.then(fn);
-//                return promise;
-//            }
-//            promise.error = function(fn) {
-//                promise.then(null, fn);
-//                return promise;
-//            }
-//            return promise;
-//        }
-//    }
-//})
 
-.factory('Locations', function($http,Accounts) {
+.factory('Locations', function($http,Accounts,User,ApiEndpoint) {
 	// Might use a resource here that returns a JSON array
 
 	// Some fake testing data
-	var locations = [ {
-		id : 1,
-		location : {
-			mlid : 1,
-			merchant_name : 'Target',
-			street : '172 Foster City Blvd',
-			city : 'Foster City',
-			state : 'CA',
-			zip : '94070',
-			lat : 403,
-			long : 300
-		},
-		name : 'Target FC Blvd',
-		amount : '30.38',
-		txn_date : '11/10/2015',
-		account_id : 1
-	},
-	{
-		id : 1,
-		location : {
-			mlid : 1,
-			merchant_name : 'The Office',
-			street : '',
-			city : 'San Carlos',
-			state : 'CA',
-			zip : '94070',
-			lat : 403,
-			long : 300
-		},
-		name : 'Office Bar',
-		amount : '30.38',
-		txn_date : '11/10/2015',
-		account_id : 1
-	},
-
-	];
+	var locations = [];
 
 	return {
+		
 		all : function() {
-			var accounts = Accounts.all();
-			var accounts_map = {}
-			for (var a in accounts){
-				accounts_map[a.id] = a;
-			}
-			for (l in locations){
-				l['account'] = accounts_map[l.account_id];
-			}
-			return locations;
+			return $http.get(ApiEndpoint.url+"/"+User.id()).then(function(response){
+				locations = response.data;
+				return locations;
+			},function(err){
+				console.error('ERR',JSON.stringify( err));
+				return [];
+			});
 		},
 		get : function(id) {
 			for (var i = 0; i < locations.length; i++) {
@@ -120,7 +61,7 @@ angular.module('starter.services', [])
 
 })
 
-.factory('UserAuth', function(User) {
+.factory('UserAuth', function($window) {
 	var users = [
 	     {
 			id : 1,
@@ -153,8 +94,20 @@ angular.module('starter.services', [])
 			for (var i=0;i<users.length;i++){
 				u = users[i];
 				if (u['email'] == e){
-					User.set_user(u);
+					$window.localStorage['userId'] = ""+u.id;
 					return u;
+				}
+			}
+			return null;
+		},
+		get : function(){
+			var uid = parseInt($window.localStorage['userId'] || "-1");
+			if (uid > 0){
+				for (var i=0;i<users.length;i++){
+					u = users[i];
+					if (u['id'] == uid){
+						return u;
+					}
 				}
 			}
 			return null;
@@ -162,34 +115,35 @@ angular.module('starter.services', [])
 	};
 	
 })
-.factory('User', function() {
-	var user = null;
+.factory('User', function(UserAuth) {
 
 	return {
-		set_user: function(u){
-			user = u;
+		
+		
+		id: function(){
+			var u = UserAuth.get();
+			if (u == null){
+				return -1;
+			}
+			return u.id;
 		},
 		
-		is_authenticated : function(){
-			return user != null;
-		},
 		settings : function(){
-			if (this.is_authenticated()){
-				return user.settings;
+			var u = UserAuth.get();
+			if (u != null){
+				return u.settings;
 			}
 			return {
 				notifications : false,
 				wow_points : -1
 			}
 		},
-		display_name : function(){
-			if (this.is_authenticated()){
-				return user.first_name;
-			}
-			return 'anonymous';
-		},
+		
 		increment_wow(num){
-			user.settings.wow_points += num;
+			var u = UserAuth.get();
+			if (u != null){
+				u.settings.wow_points += num;
+			}
 		},
 		
 		logoff : function(){
